bd2ccc6b27d3c45f39a584ed4b930156fc23c32f
DESKTOP-9SU9ME3\USER <ljm920604@gmail.com>
2025-09-22 22:58:50 +0900

    Feat : Project Setting 진행 중

---
+++ auth/src/main/java/com/payflow/auth/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.payflow.auth.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/signup")
+    public String signup() {
+        return "test";
+    }
+}
