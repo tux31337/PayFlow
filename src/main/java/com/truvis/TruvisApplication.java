c5a70d659048341a28078ec96e586c722f54f48f
DESKTOP-9SU9ME3\USER <ljm920604@gmail.com>
2025-09-29 22:04:51 +0900

    Feat: 파일명 수정

--- src/main/java/com/truvis/TruvisApplication.java
+++ src/main/java/com/truvis/TruvisApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.truvis"})
-public class truvisApplication {
+public class TruvisApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(truvisApplication.class, args);
+		SpringApplication.run(TruvisApplication.class, args);
 	}
 
 }
